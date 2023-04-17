28b879c80a4b817bad7c8ce0fe8d34a0f5d3c8e7
Poksi <Poksi@DESKTOP-5EO14RQ>
2023-04-17 20:09:52 +0200

    Set a range for the Amount

--- ExpenseCalculator/Models/Transaction.cs
+++ ExpenseCalculator/Models/Transaction.cs
@@ -7,8 +7,12 @@ namespace ExpenseCalculator.Models
     {
         [Key]
         public int TransactionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Select a category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
